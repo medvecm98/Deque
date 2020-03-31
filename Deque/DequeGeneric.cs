a284d5d10e52a68ccaec977bdd0b54075f432b86
Michal Medvecky <miskomedvecky@gmail.com>
2020-03-31 20:44:22 +0200

    Add project files.

---
+++ Deque/DequeGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deque
+{
+    interface IDeque<T> : IList<T>
+    {
+        void AddFront(T input);
+        void AddBack(T input);
+        T GetFront();
+        T GetBack();
+    }
+    public class Deque<T> : IDeque<T>
+    {
+    }
+}
