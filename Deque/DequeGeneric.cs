d65c1575cf49d79bbe0ee03e43e02ec0dce8fc8c
Michal Medvecky <miskomedvecky@gmail.com>
2020-04-01 10:17:23 +0200

    Added some properties and new struct.

--- Deque/DequeGeneric.cs
+++ Deque/DequeGeneric.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+//TODO: struct na reprezentaciu citacich hlav s pouzitim With metod
 
-namespace Deque
+interface IDeque<T> : IList<T>
 {
-    interface IDeque<T> : IList<T>
-    {
-        void AddFront(T input);
-        void AddBack(T input);
-        T GetFront();
-        T GetBack();
-    }
-    public class Deque<T> : IDeque<T>
-    {
-    }
+    T[][] QueueMap { get; set; }
+    void AddFront(T input);
+    void AddBack(T input);
+    T GetFront();
+    T GetBack();
 }
+
+internal struct HeadPosition
+{
+    public int MapPosition;
+    public byte DataBlockOffset;
+}
+
+public class Deque<T> : IDeque<T>
+{
+    const int DATABLOCK_LENGTH = 256;
+    const int DEFAULT_MAP_SIZE = 16;
+
+    int Size { get; set; }
+    HeadPosition FrontHead { get; set; }
+    HeadPosition RearHead { get; set; }
+}
+
