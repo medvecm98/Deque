7a230d7ba8b1f9dfa5bec0a60e596335af182bdb
Michal Medvecky <miskomedvecky@gmail.com>
2020-04-01 11:40:38 +0200

    Added deque sandbox.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque<int> d = new Deque<int>();
+            d.AddFront(4);
+        }
+    }
+}
